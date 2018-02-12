848ffb72fc1f95767734414cbe5133853f72afd8
Anton Greybo <greybo@Mac-mini-Anton.local>
2018-02-12 19:07:42 +0300

    Иcправил Redme и переменные

--- pprint_json.py
+++ pprint_json.py
@@ -8,10 +8,10 @@ def load_data(filepath):
     load_json = json.load(open(str(filepath)))
     return load_json
 
-data = load_data(filepath)
+json_data = load_data(filepath)
 
-def pretty_print_json(data):
-    parse_json = json.dumps(data, indent=4, sort_keys=True, ensure_ascii=False, separators=(',', ': '))
+def pretty_print_json(json_data):
+    parse_json = json.dumps(json_data, indent=4, sort_keys=True, ensure_ascii=False, separators=(',', ': '))
     return parse_json
 
-print (pretty_print_json(data))
+print (pretty_print_json(json_data))
