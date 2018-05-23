6e9a1ad70e7588d9029a61b7eb7a24a417126e2f
greybo <greiboas@gmail.com>
2018-05-23 19:25:29 +0300

    убрал общие ошибки, поставил для того чтоб не пропустить  сделал функцию проще

--- pprint_json.py
+++ pprint_json.py
@@ -4,22 +4,16 @@ import sys
 
 filepath = sys.argv[1]
 
-try:
-    def load_data(filepath):
+
+def pretty_print_json(filepath):
+    try:
         with open(filepath, 'r') as file_handler:
-            return json.load(file_handler)
+            return  json.dumps(json.load(file_handler), indent=4, ensure_ascii=False, separators=(',', ': '))
+    except ValueError as e:
+        print('Json code unccorect or file not format json. Error: ', e)
 
-    def pretty_print_json(filepath):
-        json_data = load_data(filepath)
-        parse_json = json.dumps(json_data, indent=4, ensure_ascii=False, separators=(',', ': '))
-        return parse_json
-
-    if __name__ == '__main__':
-        print (pretty_print_json(filepath))
-
-
-except ValueError as e:
-    print ('Json code unccorect or file not format json. Error: ',e)
+if __name__ == '__main__':
+    print (pretty_print_json(filepath))
 
 
 
