2c15a56572d285c018cdab9a427a39b6a9c8b4e7
greybo <greiboas@gmail.com>
2018-05-25 10:30:34 +0300

    Разбил на две функции, добавил exception

--- pprint_json.py
+++ pprint_json.py
@@ -1,19 +1,24 @@
 import json
 import sys
 
-
-filepath = sys.argv[1]
-
-
-def pretty_print_json(filepath):
+def load_data(filepath):
     try:
         with open(filepath, 'r') as file_handler:
-            return  json.dumps(json.load(file_handler), indent=4, ensure_ascii=False, separators=(',', ': '))
-    except ValueError as e:
-        print('Json code unccorect or file not format json. Error: ', e)
+            return json.load(file_handler)
+    except ValueError as error:
+        print('Json code unccorect or file not format json. Error: ', error)
+    except FileNotFoundError as error:
+        print('File not found: ', error)
+
+def pretty_print_json(filepath):
+    json_data = load_data(filepath)
+    parse_json = json.dumps(json_data, indent=4, ensure_ascii=False, separators=(',', ': '))
+    return parse_json
 
 if __name__ == '__main__':
+    filepath = sys.argv[1]
     print (pretty_print_json(filepath))
+
 
 
 
