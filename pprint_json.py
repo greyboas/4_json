705331a6c427183c4651390d2189683b12d39d85
greybo <greiboas@gmail.com>
2018-05-25 10:43:44 +0300

    Проверил на pep8

--- pprint_json.py
+++ pprint_json.py
@@ -1,6 +1,7 @@
 import json
 import sys
 
+
 def load_data(filepath):
     try:
         with open(filepath, 'r') as file_handler:
@@ -10,15 +11,14 @@ def load_data(filepath):
     except FileNotFoundError as error:
         print('File not found: ', error)
 
+
 def pretty_print_json(filepath):
     json_data = load_data(filepath)
-    parse_json = json.dumps(json_data, indent=4, ensure_ascii=False, separators=(',', ': '))
+    parse_json = json.dumps(json_data, indent=4,
+                            ensure_ascii=False, separators=(',', ': '))
     return parse_json
+
 
 if __name__ == '__main__':
     filepath = sys.argv[1]
-    print (pretty_print_json(filepath))
-
-
-
-
+    print(pretty_print_json(filepath))
