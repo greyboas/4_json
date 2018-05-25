07f8d9acbb44ef385deda3977d6ece07d3724d7e
greybo <greiboas@gmail.com>
2018-05-26 00:01:01 +0300

    26.05.2018

--- pprint_json.py
+++ pprint_json.py
@@ -3,22 +3,25 @@ import sys
 
 
 def load_data(filepath):
-    try:
-        with open(filepath, 'r') as file_handler:
-            return json.load(file_handler)
-    except ValueError as error:
-        print('Json code unccorect or file not format json. Error: ', error)
-    except FileNotFoundError as error:
-        print('File not found: ', error)
+        try:
+            with open(filepath, 'r') as file_handler:
+                return json.load(file_handler)
+        except FileNotFoundError as error:
+            print('File not found: ', error)
+        except ValueError as error:
+            print('Json code unccorect or file not format json. Error: ', error)
 
 
 def pretty_print_json(filepath):
-    json_data = load_data(filepath)
-    parse_json = json.dumps(json_data, indent=4,
+    data = load_data(filepath)
+    parse_text = json.dumps(data, indent=4,
                             ensure_ascii=False, separators=(',', ': '))
-    return parse_json
+    print(parse_text)
 
 
 if __name__ == '__main__':
-    filepath = sys.argv[1]
-    print(pretty_print_json(filepath))
+    try:
+        filepath = sys.argv[1]
+    except IndexError as error:
+        filepath = 'example_bar.json'
+    pretty_print_json(filepath)
