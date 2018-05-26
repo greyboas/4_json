3a977de6f8e87f501ea6740196dd5a9ab193c3ae
greybo <greiboas@gmail.com>
2018-05-26 23:46:47 +0300

    вроде поправил 1) перенес из функции обработку 2) переименовал переменные 3) расширил ifmain 4) поправил оформление

--- pprint_json.py
+++ pprint_json.py
@@ -6,22 +6,29 @@ def load_data(filepath):
     try:
         with open(filepath, 'r') as file_handler:
             return json.load(file_handler)
-    except FileNotFoundError as error:
-        print('File not found: ', error)
     except ValueError as error:
-        print('Json code unccorect or file not format json. Error: ', error)
+        return 'It is not file fotmat JSON'
 
 
-def pretty_print_json(filepath):
-    json_load = load_data(filepath)
-    parse_text = json.dumps(json_load, indent=4,
-                            ensure_ascii=False, separators=(',', ': '))
-    print(parse_text)
+def pretty_print_json(data):
+    json_viewer = json.dumps(
+        data,
+        indent=4,
+        ensure_ascii=False,
+        separators=(',', ': '))
+    print(json_viewer)
 
 
 if __name__ == '__main__':
     try:
         filepath = sys.argv[1]
+        file = open(filepath)
     except IndexError as error:
-        filepath = 'example_bar.json'
-    pretty_print_json(filepath)
+        print('Not set argyment filepath')
+        sys.exit(-1)
+    except FileNotFoundError as error:
+        print('File not found: ', error)
+        sys.exit(-1)
+    else:
+        data = load_data(filepath)
+        pretty_print_json(data)
